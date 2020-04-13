2569ddda4918fef58ed39ac4e829fac717d58451
tommy <tommy.lorentz@gmail.com>
2020-04-13 20:02:27 +0800

    add video78, 79

--- 75.stm32f446re_drivers/drivers/inc/stm32f446xx.h
+++ 75.stm32f446re_drivers/drivers/inc/stm32f446xx.h
@@ -9,6 +9,14 @@
 #ifndef INC_STM32F446XX_H_
 #define INC_STM32F446XX_H_
 
+/*
+ * base addresses of Flash and SRAM memories
+ */
+#define FLASH_BASEADDR		0x08000000U
+#define SRAM1_BASEADDR		0x20000000U //112KB
+#define SRAM2_BASEADDR		0x2001C000U
+#define ROM_BASEADDR		0x1FFF0000U
+#define SRAM_ADDR			SRAM1_BASEADDR
 
 
 
